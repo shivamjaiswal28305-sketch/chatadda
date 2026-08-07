1b0ccce42359ce570d07cce8cb9fdd2429bc6c93
shivamjaiswal28305-sketch <shivamjaiswal28305@gmail.com>
2026-08-07 05:43:04 +0530

    Add photoUrl field to User model

--- models/User.js
+++ models/User.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
   lastSeen: {
     type: Date,
     default: Date.now
+  },
+  photoUrl: {
+    type: String,
+    default: ''
   }
 }, { timestamps: true });
 
